fa43547aa55606a30f482b0ae6c3f842c2ccc1c4
STRXI01 <rizalpro598@gmail.com>
2025-11-15 01:15:20 +0530

    Update start.py

--- OpusV/utils/inline/start.py
+++ OpusV/utils/inline/start.py
@@ -4,23 +4,31 @@ from OpusV import app
 
 
 def start_panel(_):
-    return [[InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHANNEL)]]
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+    ]
+    return buttons
 
 
 def private_panel(_):
-    return [
+    buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true&admin=delete_messages+invite_users",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_8"], callback_data="LG"),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(
+                text=_["S_B_13"],
+                url=f"https://t.me/{app.username}?startapp=true",
+            ),           
         ],
-        [
-            InlineKeyboardButton(text=_["S_B_3"], callback_data="open_help"),
-        ],
- 
-    ]
+    ]      
+    return buttons
